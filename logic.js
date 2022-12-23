acffebc3f92f4856fbac111a03d6dcd8040f417b
Priyanshu Agnihotri <116781482+Priyanshu871@users.noreply.github.com>
2022-12-23 12:27:27 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,21 +1,39 @@
-var currentDateTime = new Date();
-var year = currentDateTime.getFullYear();
-var month = (currentDateTime.getMonth() + 1);
-var date = (currentDateTime.getDate() + 1);
+function validateform() {
+    var returnval = true;
 
-if (date < 10) {
-    date = '0' + date;
+    var name = document.forms['RegForm']["name"].value;
+    if (name.length == 0 || name == null || name == " ") {
+        alert("Name *Can't be Empty");
+        returnval = false;
+    }
+
+    else if (name.length < 3) {
+        alert("Name *Length of name is too short");
+        returnval = false;
+    }
+
+    var phone = document.forms['RegForm']["phone"].value;
+    if (phone.length != 10 || phone == null || phone == " ") {
+        alert("Phone *Phone number should be of 10 digits!");
+        returnval = false;
+    }
+
+    var email = document.forms['RegForm']["email"].value;
+    if (email.length > 35 || email == null || email == " ") {
+        alert("Email *Email length is too long");
+        returnval = false;
+    }
+    // var Address = document.forms['RegForm']["Address"].value;
+    // if (Address.length < 10 || Address == null || Address == " ") {
+    //     alert("Address *Enter Address Properly");
+    //     returnval = false;
+    // }
+
+    // document.getElementById("abcd1").innerHTML = Math.floor(Math.random() * 10);
+    return returnval;
 }
-if (month < 10) {
-    month = '0' + month;
-}
 
-var dateTomorrow = year + "-" + month + "-" + date;
-var checkinElem = document.querySelector("#checkin-date");
-var checkoutElem = document.querySelector("#checkout-date");
 
-checkinElem.setAttribute("min", dateTomorrow);
 
-checkinElem.onchange = function () {
-    checkoutElem.setAttribute("min", this.value);
-}
+
+
